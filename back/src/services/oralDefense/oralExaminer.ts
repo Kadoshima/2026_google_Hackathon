895b84d32fbfaee533b796966796d9feeaee0f25
AtsukiKiry <ep22059-1856@sti.chubu.ac.jp>
2026-02-12 22:30:47 +0900

    BE070

--- back/src/services/oralDefense/oralExaminer.ts
+++ back/src/services/oralDefense/oralExaminer.ts
@@ -1,2 +1,14 @@
-// TODO: implement oral examiner
-export {}
+import type { OralAskRequest, OralAskResponse } from 'shared'
+
+export const nextQuestion = async (
+  analysisId: string,
+  context?: OralAskRequest['context']
+): Promise<OralAskResponse> => {
+  void analysisId
+  void context
+
+  return {
+    question: 'Please summarize the core novelty of your claim in one sentence.',
+    follow_up: false
+  }
+}
